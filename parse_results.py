9e508d05b6bff35e24712edd1e0cc90f6d8b2965
darkhorrow <benearosemidan@gmail.com>
2021-03-15 20:03:55 +0000

    Upload the correct file

--- parse_results.py
+++ parse_results.py
@@ -1,5 +1,6 @@
 import argparse
 from statistics import NormalDist
+import re
 
 
 def confidence_interval(data, confidence=0.95):
@@ -20,14 +21,15 @@ with open(args.i, 'r') as input:
     init_time = []
     exec_time = []
     for line in input.readlines():
-        for token in line.split():
-            try:
-                if i % 2 == 0:
-                    init_time.append(float(token))
-                else:
-                    exec_time.append(float(token))
-            except ValueError:
-                pass
+        token = re.split('seg|:', line)
+        try:
+            if i % 2 == 0:
+                init_time.append(float(token[1]))
+            else:
+                exec_time.append(float(token[1]))
+            i = i + 1
+        except ValueError:
+            pass
 
     print(confidence_interval(init_time))
     print(confidence_interval(exec_time))
